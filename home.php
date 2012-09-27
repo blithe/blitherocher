44dac26e478b7f085a38e9b8303b47f9fa2f4f99
Blithe <blitherocher@gmail.com>
2012-09-27 14:33:04 -0700

    added coderwall link

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<p>I'm a <a href='http://www.linkedin.com/in/blitherocher'>scientist</a> and a <a href='http://app.strava.com/athletes/13682'>cyclist</a> living in Atlanta, Georgia.<br/>
+<p>I'm a <a href='http://www.linkedin.com/in/blitherocher'>scientist</a>, a <a href='http://coderwall.com/blithe'>coder</a>, and a <a href='http://app.strava.com/athletes/13682'>cyclist</a> living in Atlanta, Georgia.<br/>
 </p>
 
 <p>Follow me on <a class='twitter' href='http://twitter.com/blithe'>twitter</a>, view my posts on <a class='tumblr' href='http://blitherocher.tumblr.com'>tumblr</a>,<br/> or send me an <a class='email' href='mailto:blithe@blitherocher.com'>email</a>.
